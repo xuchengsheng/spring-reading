47b58363f9eb8dc27b201e71c448e800b1f95443
linlei <linlei@ehompoct.com>
2024-04-11 13:51:15 +0800

    删除advice接口源码

--- spring-aop/spring-aop-advice/src/main/java/com/xcs/spring/MyMonitoringCapable.java
+++
@@ -1,5 +0,0 @@
-package com.xcs.spring;
-
-public interface MyMonitoringCapable {
-    void toggleMonitoring();
-}
--- spring-aop/spring-aop-advice/src/main/java/com/xcs/spring/MyThrowsAdvice.java
+++
@@ -1,9 +0,0 @@
-package com.xcs.spring;
-
-import org.springframework.aop.ThrowsAdvice;
-
-public class MyThrowsAdvice implements ThrowsAdvice {
-    public void afterThrowing(Exception ex) throws Throwable {
-        System.out.println("Exception thrown: " + ex.getMessage());
-    }
-}
